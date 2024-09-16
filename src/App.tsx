5581c152cf5cf674ad850629f5524e6c579875f2
bhingarde1 <pranavbhingarde1998@gmail.com>
2024-09-16 17:42:41 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import MiniDrawer from './Pages/Layout';
+import Demo from './Pages/Demo/Demo';
+
 
 function App() {
   return (
-    <div>app</div>
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<MiniDrawer />}>
+          <Route index element={<Home />} />
+          <Route path='/demo' element={<Demo />} />
+        </Route>
+
+
+      </Routes>
+
+    </BrowserRouter>
+    // <div>app</div>
   );
 }
 
---
+++ src/Pages/Demo/Demo.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Demo = () => {
+  return (
+    <div>
+         </div>
+  )
+}
+
+export default Demo
