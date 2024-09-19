17b2a9abfd5f0341419cf71f131d5cb5f2ec6c15
yash-gole28 <yashgole2806@gmail.com>
2024-09-19 17:54:52 +0530

    added Category wise data table

--- src/Components/KPIs.tsx
+++ src/Components/KPIs.tsx
@@ -7,7 +7,7 @@ import Paper from '@mui/material/Paper';
 import data from './../Data.json'
 
 const KPIs = () => {
-    const nonAllocatedAssets = data.totalAssets - (data.totalApprovedAssets + data.assetsUnderMaintenance) 
+    
     const Item = styled(Paper)(({ theme }) => ({
         boxShadow: '0px 0px 0px #fff',
         backgroundColor: '#fff',
@@ -21,23 +21,37 @@ const KPIs = () => {
     }));
     const CustomGrid = styled(Paper)(({ theme }) => ({
         boxShadow: '0px 0px 0px #fff',
-        minHeight: '100px',
+        height:'100px',
+        [theme.breakpoints.down('sm')]: {
+            height:'60px'
+          },
         backgroundColor: '#fff',
 
     }));
+    let totalCounts = 0;
+    let allocatedCount = 0;
+    let underMaintenanceCount = 0;
+  
+    data.CategoryData.forEach(item => {
+      totalCounts += parseInt(item.count);
+      allocatedCount += parseInt(item.allocated);
+      underMaintenanceCount += parseInt(item.underMaintenance);
+    });
+
+    const nonAllocatedAssets = totalCounts - (allocatedCount + underMaintenanceCount) 
 
     return (
         <Box sx={{ p: { xs: 1, sm: 1, md: 2 } }}>
             <Grid container spacing={{ xs: 1, sm: 1, md: 2 }}>
-                <Grid size={{ xs: 6, md: 3 }}>
+                <Grid size={{ xs: 12,sm:6, md: 3 }}>
                     <Item>
                         <CustomGrid>
                             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
-                                <Typography variant='h6'>
+                                <Typography variant='h6' sx={{fontSize:"15px"}}>
                                     Total assets
                                 </Typography>
-                                <Typography variant='h3'>
-                                    {data.totalAssets}
+                                <Typography variant='h3' sx={{fontSize:{xs:'22px',sm:'25px',md:'35px'}}}>
+                                    {totalCounts}
                                 </Typography>
 
                             </Box>
@@ -45,44 +59,44 @@ const KPIs = () => {
                         </CustomGrid>
                     </Item>
                 </Grid>
-                <Grid size={{ xs: 6, md: 3 }}>
+                <Grid size={{ xs: 12,sm:6, md: 3 }}>
                     <Item>
                         <CustomGrid>
                             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
-                                <Typography variant='h6'>
+                                <Typography variant='h6' sx={{fontSize:"15px"}}>
                                     Total assets
                                 </Typography>
-                                <Typography variant='h3'>
-                                    {data.totalApprovedAssets}
+                                <Typography variant='h3' sx={{fontSize:{xs:'22px',sm:'25px',md:'35px'}}}>
+                                    {allocatedCount}
                                 </Typography>
 
                             </Box>
                         </CustomGrid>
                     </Item>
                 </Grid>
-                <Grid size={{ xs: 6, md: 3 }}>
+                <Grid size={{ xs: 12,sm:6, md: 3 }}>
                     <Item>
                         <CustomGrid>
                             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
-                                <Typography variant='h6'>
+                                <Typography variant='h6' sx={{fontSize:"15px"}}>
                                     Assets under Maintenance
                                 </Typography>
-                                <Typography variant='h3'>
-                                    {data.assetsUnderMaintenance}
+                                <Typography variant='h3' sx={{fontSize:{xs:'22px',sm:'25px',md:'35px'}}}>
+                                    {underMaintenanceCount}
                                 </Typography>
 
                             </Box>
                         </CustomGrid>
                     </Item>
                 </Grid>
-                <Grid size={{ xs: 6, md: 3 }}>
+                <Grid size={{ xs: 12,sm:6, md: 3 }}>
                     <Item>
                         <CustomGrid>
                             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
-                                <Typography variant='h6'>
+                                <Typography variant='h6' sx={{fontSize:"15px"}}>
                                     available assets
                                 </Typography>
-                                <Typography variant='h3'>
+                                <Typography variant='h3' sx={{fontSize:{xs:'22px',sm:'25px',md:'35px'}}}>
                                     {nonAllocatedAssets}
                                 </Typography>
 
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,66 @@
-
 import React from 'react'
-import Login from '../Login/Login'
 import KPIs from '../../Components/KPIs'
+import data from './../../Data.json'
+import { Box, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 
 const Home = () => {
+
+
   return (
     <>
-     <KPIs/>
+      <KPIs />
+      <Box>
+        <Box sx={{ width: '95%', margin: 'auto' }}>
+          {/* Button to register new assets */}
+          {/* <Box sx={{ display: "flex", alignItems: 'center', justifyContent: 'flex-end', marginBottom: '10px', mt: '2rem' }}>
+            <Button variant="outlined">Register New Assets</Button>
+          </Box> */}
+
+
+
+
+          <TableContainer sx={{
+            width: { xs: "100%", sm: "100%", },
+            borderRadius: "10px",
+            backgroundColor: "white",
+            boxShadow: 2,
+            marginBottom: "20px",
+            position: "relative",
+          }}>
+            <Typography sx={{ margin: "10px", fontWeight: 'bold', fontSize: '1.2rem' }}>Asset Requests</Typography>
+
+            <Table sx={{ width: '100%' }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="left">Category</TableCell>
+                  <TableCell align="left">Total Assets Count</TableCell>
+                  <TableCell align="right">Allocated Assets</TableCell>
+                  <TableCell align="right">Assets under Maintenance</TableCell>
+                  <TableCell align="right">Non Allocated Assets</TableCell>
+                  {/* <TableCell align="right">Status</TableCell> */}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.CategoryData.map((v, index) => (
+                  <TableRow key={index}>
+                    <TableCell align="left">{v.category}</TableCell>
+                    <TableCell align="left">{v.count}</TableCell>
+                    <TableCell align="right">{v.allocated}</TableCell>
+                    <TableCell align="right">{v.underMaintenance}</TableCell>
+                    <TableCell align="right">{Number(v.count)-(Number(v.allocated) + Number(v.underMaintenance))}</TableCell>
+                    {/* <TableCell align="right">{v.approved == 'pending' ? <Box> <Button>approve</Button> <Button>reject</Button> </Box> : v.approved}</TableCell> */}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+
+
+            
+          </TableContainer>
+
+          {/* Table Container */}
+        </Box>
+      </Box>
     </>
   )
 }
