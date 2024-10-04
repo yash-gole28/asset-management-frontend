b396712b8a63b30cd619151d82944284f1924d4f
bhingarde1 <pranavbhingarde1998@gamil.com>
2024-10-04 15:27:09 +0530

    kPI CHANGES

--- src/Components/KPIs.tsx
+++ src/Components/KPIs.tsx
@@ -51,10 +51,10 @@ const KPIs = () => {
                     <Item sx={{ backgroundColor: '', position: 'relative', overflow: 'hidden' }}>
                         <CustomGrid>
                             <Box sx={{ pl: { xs: '0px', sm: '1rem', md: '1rem' }, display: 'flex', flexDirection: 'column', alignItems: 'start', justifyContent: 'center', height: '100%' }}>
-                                <Typography variant='h6' sx={{ fontSize: "15px" }}>
+                                <Typography variant='h6' sx={{ fontSize: "14px" }}>
                                     Total assets
                                 </Typography>
-                                <Typography variant='h3' sx={{ fontSize: { xs: '22px', sm: '25px', md: '35px' } }}>
+                                <Typography variant='h3' sx={{ fontSize: { xs: '18px', sm: '20px', md: '25px' } }}>
                                     {totalCounts}
                                 </Typography>
                                 <InventoryIcon sx={{ fontSize: { xs: '50px', sm: '50px', md: '60px' }, color: 'rgb(250, 176, 68)', borderRadius: '50%', position: 'absolute', right: '15px', backgroundColor: 'rgb(255, 241, 218)', padding: '1rem' }} />
@@ -67,10 +67,10 @@ const KPIs = () => {
                     <Item sx={{ position: 'relative', overflow: 'hidden' }}>
                         <CustomGrid>
                             <Box sx={{ pl: { xs: '0px', sm: '1rem', md: '1rem' }, display: 'flex', flexDirection: 'column', alignItems: 'start', justifyContent: 'center', height: '100%' }}>
-                                <Typography variant='h6' sx={{ fontSize: "15px" ,zIndex:"100"}}>
+                                <Typography variant='h6' sx={{ fontSize: "13px" ,zIndex:"100"}}>
                                     Allocated Assets
                                 </Typography>
-                                <Typography variant='h3' sx={{ fontSize: { xs: '22px', sm: '25px', md: '35px' } }}>
+                                <Typography variant='h3' sx={{ fontSize: { xs: '18px', sm: '20px', md: '25px' } }}>
                                     {allocatedCount}
                                 </Typography>
                                 <AssignmentIndIcon sx={{ fontSize: { xs: '50px', sm: '50px', md: '60px' }, color: 'rgb(118, 122, 245)',
@@ -83,10 +83,10 @@ const KPIs = () => {
                     <Item sx={{  position: 'relative', overflow: 'hidden' }}>
                         <CustomGrid>
                             <Box sx={{ pl: { xs: '0px', sm: '1rem', md: '1rem' }, display: 'flex', flexDirection: 'column', alignItems: 'start', justifyContent: 'center', height: '100%' }}>
-                                <Typography variant='h6' sx={{ fontSize: "15px", textAlign: 'start',zIndex:"100" }}>
-                                    Assets under Maintenance
+                                <Typography variant='h6' sx={{ fontSize: "13px", textAlign: 'start',zIndex:"100" }}>
+                                     Under Maintenance
                                 </Typography>
-                                <Typography variant='h3' sx={{ fontSize: { xs: '22px', sm: '25px', md: '35px' } }}>
+                                <Typography variant='h3' sx={{ fontSize: { xs: '18px', sm: '20px', md: '25px' } }}>
                                     {underMaintenanceCount}
                                 </Typography>
                                 <BuildIcon sx={{ fontSize: { xs: '50px', sm: '50px', md: '60px' }, position: 'absolute',padding: '1rem', right: '15px',borderRadius: '50%',color:"rgb(240, 158, 127)",backgroundColor: 'rgb(253, 234, 228)' }}  />
@@ -98,10 +98,10 @@ const KPIs = () => {
                     <Item sx={{  position: 'relative', overflow: 'hidden' }}>
                         <CustomGrid>
                             <Box sx={{ pl: { xs: '0px', sm: '1rem', md: '1rem' }, display: 'flex', flexDirection: 'column', alignItems: 'start', justifyContent: 'center', height: '100%' }}>
-                                <Typography variant='h6' sx={{ fontSize: "15px",zIndex:"100" }}>
+                                <Typography variant='h6' sx={{ fontSize: "13px",zIndex:"100" }}>
                                     Available Assets
                                 </Typography>
-                                <Typography variant='h3' sx={{ fontSize: { xs: '22px', sm: '25px', md: '35px' } }}>
+                                <Typography variant='h3' sx={{ fontSize: { xs: '18px', sm: '20px', md: '25px' } }}>
                                     {nonAllocatedAssets}
                                 </Typography>
                                 <KeyboardIcon sx={{ fontSize: { xs: '50px', sm: '50px', md: '60px' },padding: '1rem',  right: '15px',borderRadius: '50%',color:"rgb(133, 136, 246)",backgroundColor: 'rgb(253, 234, 228)', position: 'absolute' }} />
